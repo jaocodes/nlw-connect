9c64aba1d03ab33ecdeb88206520d24c67a95edf
jaocodes <jhonskaz@gmail.com>
2025-02-19 10:30:20 -0300

    adiciona configuração do ambiente e refatora servidor Fastify

---
+++ src/env.ts
@@ -0,0 +1,15 @@
+import z from 'zod'
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Environment validation error:', _env.error.format())
+
+  throw new Error('Environment validation error')
+}
+
+export const env = _env.data
--- src/routes/subscribe-to-event.ts
+++ src/routes/subscribe-to-event.ts
@@ -1,29 +1,32 @@
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import z from "zod";
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
 
-export const subscribeToEvent: FastifyPluginAsyncZod =  async (app) => {
-    app.post('/subscriptions', {
-        schema: {
-            summary: 'Subscribe someone to the event',
-            tags: ['subscription'],
-            body: z.object({
-                name: z.string(),
-                email: z.string().email()
-            }),
-            response:{
-                201: z.object({
-                    name: z.string(),
-                    email: z.string().email()
-                })
-            }
-        }
-    }, async (request, reply) => {
-    
-        const {name, email} = request.body
-    
-    
-        return reply.status(201).send({
-            name, email
-        })
-    })
+export const subscribeToEvent: FastifyPluginAsyncZod = async (app) => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscribe someone to the event',
+        tags: ['subscription'],
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string().email(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body
+
+      return reply.status(201).send({
+        name,
+        email,
+      })
+    },
+  )
 }
